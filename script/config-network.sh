8a76435a1a34841d3e05cb936f1b7e4fda37deef
ming <fox20431@gmail.com>
2023-12-18 18:51:39 +0800

    update the ssh config

--- script/config-network.sh
+++ script/config-network.sh
@@ -8,7 +8,7 @@ file_path=/etc/hosts
 if ! grep -q "$hostname" "$file_path"; then
     # 如果不存在则添加字符串
     sudo echo -e "\n127.0.0.1 $(hostname)" >> /etc/hosts
-    echo "Added localhost-hostname map."
+    log "Added localhost-hostname map."
 else
-    echo "localhost-hostname already exists in $file_path"
+    log "localhost-hostname already exists in $file_path"
 fi
