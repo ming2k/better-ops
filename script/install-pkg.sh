883d81608b25e9b49ae444b9f8439669cd7893a4
ming <fox20431@gmail.com>
2024-01-01 23:34:47 +0800

    update the vimrc

--- script/install-pkg.sh
+++ script/install-pkg.sh
@@ -6,6 +6,7 @@ print_with_border "INSTALL PACKAGE"
 if [ "$(get_distribution)" = "debian" ]; then
     install_package apt sudo
     install_package apt build-essential
+    install_package apt rsync
     # Optinal package
     install_package apt ranger
 fi
