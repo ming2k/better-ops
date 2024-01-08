4ea713f3f736f43db5cc0f86d7c771248715df2f
ming <fox20431@gmail.com>
2024-01-08 20:44:50 +0800

    remove the banner space line and add the setting vim for archlinux

--- lib/generate-banner.sh
+++ lib/generate-banner.sh
@@ -5,9 +5,6 @@ print_with_border() {
   cols=64
   padding=$((($cols-2-${#str})/2))
   
-  echo
-  echo
-  echo
   # printf '+'; printf -- '-%.0s' {1..$(($cols-2))}; printf '+'; echo
   printf '+'; printf -- '-%.0s' $(seq 1 $(($cols-2))); printf '+'; echo
   printf '|'; printf "%${padding}s"; printf "$str"; printf "%${padding}s"; printf '|'; echo
--- script/config-vim.sh
+++ script/config-vim.sh
@@ -16,3 +16,16 @@ if [ "$(get_distribution)" = "debian" ]; then
     log "Finish"
 fi
 
+# app should be installed
+if [ "$(get_distribution)" = "arch" ]; then
+    install_package pacman vim
+    vimrc_path=/etc/vimrc
+    sudo rm $vimrc_path
+    # Add the os-related config
+    sudo cat 'runtime! archlinux.vim' > $vimrc_path
+    sudo cat $asset_path/vimrc > $vimrc_path
+
+    sudo echo -e "\nexport EDITOR=vim" > /etc/bash.bashrc
+
+    log "Finish"
+fi
