ff396806cd3f5fe8c835e3ef1b4bb0714bce0871
ming <fox20431@gmail.com>
2024-05-22 19:30:31 +0800

    fix the logic error

--- src/lib/install-package.sh
+++ src/lib/install-package.sh
@@ -4,18 +4,18 @@
 install_package() {
     # 检查是否输入了包管理工具和软件包名称
     if [ -z "$1" ] || [ -z "$2" ]; then
-        echo "Please specify the package management tool and package name."
+        log "error" "Please specify the package management tool and package name."
         return 1
     fi
 
     # 检查当前用户是否为 root 用户
     if [ "$EUID" -ne 0 ]; then
-        log "Password is asked to install package."
+        log "error" "Password is asked to install package."
     fi
 
     case "$1" in
         "apt")
-            dpkg -l | grep -q -w "^ii  $2 " && log "$2 has installed." & return 0
+            dpkg -l | grep -q -w "^ii  $2 " && {log "$2 has installed."; return 0}
             sudo apt-get update > /dev/null
             sudo apt-get install -y "$2" > /dev/null
             ;;
