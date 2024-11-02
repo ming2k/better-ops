79c3ee82ac40afc32aa86289f7b4bffbfb2deea6
ming <mingmillennium@gmail.com>
2024-11-02 14:48:51 +0800

    fix the error path in main.sh, rename some files

--- bin/main.sh
+++ bin/main.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-####################
-# PRESET KEY VALUE #
-####################
-
-PUB_KEY_URL=https://github.com/ming2k.keys
-
 # check if the current shell is running bash and exit if not
 if [ "$SHELL" != "/bin/bash" ]; then
     echo -e "[$(date "+%Y-%m-%d %H:%M:%S")] [\e[33mWARN\e[0m] Invalid number of arguments"
     exit 1
 fi
 
-# get the current file absolute path
-script_path=$(dirname "$(readlink -f "$0")")
+# get the parent directory of the current script
+parent_dir=$(dirname "$(dirname "$(readlink -f "$0")")")
+script_path="$parent_dir/scripts"
+lib_path="$parent_dir/lib"
 asset_path="$script_path/assets"
 
 # active the library
-lib_path="$script_path/lib"
 . $lib_path/log.sh
 . $lib_path/get_distribution.sh
 . $lib_path/install_package.sh
@@ -29,11 +24,11 @@ DIST_OS=$(get_distribution)
 debian_script_path="$script_path/distro/debian"
 if [ $DIST_OS = "debian" ]; then
     . ${debian_script_path}/preflight.sh
-    . ${debian_script_path}/config-network.sh
-    . ${debian_script_path}/config-bash.sh
-    . ${debian_script_path}/config-ssh.sh
-    . ${debian_script_path}/config-nvim.sh
-    . ${debian_script_path}/config-docker.sh
+    . ${debian_script_path}/config_network.sh
+    . ${debian_script_path}/config_bash.sh
+    . ${debian_script_path}/config_ssh.sh
+    . ${debian_script_path}/config_nvim.sh
+    . ${debian_script_path}/config_docker.sh
 fi
 
 source /etc/bash.bashrc
--- scripts/distro/debian/config_ssh.sh
+++ scripts/distro/debian/config_ssh.sh
@@ -2,9 +2,7 @@
 
 print_with_border "SETTING SSHD"
 
-wget $PUB_KEY_URL -O ~/.ssh/authorized_keys
-
-log "Already set ssh authorized_keys."
+# log "Already set ssh authorized_keys."
 
 sudo cp $asset_path/sshd_config /etc/ssh/sshd_config
 
