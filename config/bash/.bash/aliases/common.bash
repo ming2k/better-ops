efb31ee6ef0860fedfdd6f4eaab9700f43c61ae3
Ming <mingmillennium@gmail.com>
2025-11-24 13:20:52 +0800

    Add alias for bashrc

---
+++ config/bash/.bash/aliases/common.bash
@@ -0,0 +1,26 @@
+# Common aliases
+
+# ls aliases
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# grep with color
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Safety aliases
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Directory navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# Disk usage
+alias df='df -h'
+alias du='du -h'
